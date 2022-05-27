b89f1723e8aa5ef94afe566657c4d62f15155366
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-26 17:06:05 -0700

    connected to front end

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-
+const dateFormat = require("../utils/dateFormat");
 const PizzaSchema = new Schema(
   {
     pizzaName: {
@@ -11,6 +11,7 @@ const PizzaSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
@@ -27,13 +28,17 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
 );
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual("commentCount").get(function () {
-  return this.comments.length;
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 // create the Pizza model using the PizzaSchema
 const Pizza = model("Pizza", PizzaSchema);
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,9 +2,15 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 // POST callback
 router.route("/:pizzaId").post(addComment);
-// DELETE callback
+// DELETE callback for removing comment
 router.route("/:pizzaId/:commentId").delete(removeComment);
+// PUT callback to make reply
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+// DELETE callback for removing reply
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 module.exports = router;
