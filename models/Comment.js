fdb49e38419cf63a0d685c99262b89108989cd96
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-27 18:45:42 -0700

    commit before heroku

--- models/Comment.js
+++ models/Comment.js
@@ -9,9 +9,12 @@ const ReplySchema = new Schema(
     },
     replyBody: {
       type: String,
+      required: true,
+      trim: true,
     },
     writtenBy: {
       type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
@@ -29,9 +32,11 @@ const CommentSchema = new Schema(
   {
     writtenBy: {
       type: String,
+      required: true,
     },
     commentBody: {
       type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
